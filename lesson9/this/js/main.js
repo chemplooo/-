d131022577ea8e42b4bfe17ab081342c77cd4805
NickNIck <40608211+chemplooo@users.noreply.github.com>
2019-05-14 11:52:00 +0300

    lesson10

--- lesson9/this/js/main.js
+++ lesson9/this/js/main.js
@@ -1,10 +1,22 @@
-
-let age = document.getElementById('age');
-    
-showUser = function (surname, name) {
-    this.surname = surname;
-    this.name = name;
-    alert("Пользователь " + this.surname + " " + this.name + ", его возраст " + age.value);
-}
- 
-showUser('Kulibaba', 'Nikolai');
+window.addEventListener("DOMContentLoaded", function () {
+    function User(name, id) {
+      this.name = name;
+      this.hello = function () {
+        console.log(`hello ${this.name}`);
+      };
+    }
+    let input = document.getElementById('age');
+  
+    input.addEventListener("input", function () {
+      input.value = inputAge.value.replace(/[^0-9+]/, '');
+    });
+    input.addEventListener("blur", function () {
+      showUser.call(this, "Miro", "Nick");
+    });
+  
+    function showUser(surname, name) {
+      alert("Увожаемый " + surname + " " + name + ", его возраст состовляет " +
+        this.value);
+    }
+  
+  });
